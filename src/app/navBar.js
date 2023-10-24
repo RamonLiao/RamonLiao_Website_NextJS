8ea533d42c405c80e1e33b19d2d950e94ecb9773
Ramon Liao <ramonliao.yc@gmail.com>
2023-10-24 22:56:39 +0100

    update nav bar

--- src/app/navBar.js
+++ src/app/navBar.js
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styles from "./styles/navbar.module.css";
@@ -7,15 +8,26 @@ import { useState, useEffect } from "react";
 
 export default function NavBar() {
   const pathname = usePathname();
+  // const { asPath, pathname} = useRouter();
+  // console.log(asPath); // '/blog/xyz'
+  // console.log(pathname); // '/blog/[slug]'
+  // router.pathname
+  // router.asPath -> include the url query parameters
 
   const [scrollTop, setScrollTop] = useState(0);
+  const [toggleMenu, setToggleMenu] = useState(true);
 
+  // When a page is loaded, reset the window scroll
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setToggleMenu(true);
+  }, [pathname]);
+
+  // Read window scroll
   useEffect(() => {
     const handleScroll = (event) => {
       setScrollTop(window.scrollY);
-      // console.log(window.scrollY);
     };
-
     window.addEventListener("scroll", handleScroll);
 
     return () => {
@@ -23,41 +35,94 @@ export default function NavBar() {
     };
   }, []);
 
-  // const { asPath, pathname} = useRouter();
-  // console.log(asPath); // '/blog/xyz'
-  // console.log(pathname); // '/blog/[slug]'
-  // router.pathname
-  // router.asPath -> include the url query parameters
+  // Handle Toggling Menu
+  const handleToggleMenu = () => {
+    setToggleMenu(!toggleMenu);
+  };
 
-  return (
-    <nav
-      className={
-        scrollTop > 25 ? `${styles.nav} ${styles.smallNav}` : `${styles.nav}`
-      }
-    >
-      {/* <>Current pathname: {pathname}</> */}
-      <ul>
-        <li className={pathname == "/" ? `${styles.active}` : ""}>
-          <Link href="/">Home</Link>
-        </li>
-        <li className={pathname == "/about" ? `${styles.active}` : ""}>
-          <Link href="/about">About</Link>
-        </li>
-        <li className={pathname == "/projects" ? `${styles.active}` : ""}>
-          <Link href="/projects">Projects</Link>
-        </li>
-        <li className={pathname == "/contact" ? `${styles.active}` : ""}>
-          <Link href="/contact">Contact</Link>
-        </li>
-        <li>
-          <a target="_blank" href="https://kohwys.com/" className={styles.blog}>
-            Blog
-          </a>
-        </li>
-        {/* <li className={pathname == "/shop" ? `${styles.active}` : ""}>
+  // Handle NavBar Styles by status
+  const handleNavBarStyle = () => {
+    let result = "";
+    result =
+      scrollTop > 25
+        ? `${styles.navbar} ${styles.smallNavbar}`
+        : `${styles.navbar}`;
+
+    result += !toggleMenu ? `  ${styles.open}` : ``;
+
+    return result;
+  };
+
+  const iconPixel = 40;
+  const navBurger = (
+    <>
+      <button
+        type="button"
+        className={styles.navbarBurger}
+        onClick={handleToggleMenu}
+      >
+        {toggleMenu ? (
+          <span className={styles.materialIcons}>
+            <Image
+              className={styles.navbarBurgerImg}
+              src="/Icons/google_icon_menu.svg"
+              alt="menu"
+              width={iconPixel}
+              height={iconPixel}
+            />
+          </span>
+        ) : (
+          <span className={styles.materialIcons}>
+            <Image
+              className={styles.navbarBurgerImg}
+              src="/Icons/google_icon_close.svg"
+              alt="close"
+              width={iconPixel}
+              height={iconPixel}
+            />
+          </span>
+        )}
+      </button>
+    </>
+  );
+
+  const navItems = (
+    <>
+      <nav>
+        <ul>
+          <li className={pathname == "/" ? `${styles.active}` : ""}>
+            <Link href="/">Home</Link>
+          </li>
+          <li className={pathname == "/about" ? `${styles.active}` : ""}>
+            <Link href="/about">About</Link>
+          </li>
+          <li className={pathname == "/projects" ? `${styles.active}` : ""}>
+            <Link href="/projects">Projects</Link>
+          </li>
+          <li className={pathname == "/contact" ? `${styles.active}` : ""}>
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://kohwys.com/"
+              className={styles.blog}
+            >
+              Blog
+            </a>
+          </li>
+          {/* <li className={pathname == "/shop" ? `${styles.active}` : ""}>
           <Link href="/shop">Shop</Link>
         </li> */}
-      </ul>
+        </ul>
+      </nav>
+    </>
+  );
+
+  return (
+    <nav className={handleNavBarStyle()}>
+      {navItems}
+      {navBurger}
     </nav>
   );
 }
